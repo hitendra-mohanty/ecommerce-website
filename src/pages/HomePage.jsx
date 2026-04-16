fb40e70ea07ab3248e76e633114fbb6f62a9fe70
Hitendra Kumar Mohanty <mohantyhitendra78@gmail.com>
2026-04-17 04:10:33 +0530

    Added navbar and home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import { getProducts}  from "../data/products"
+import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
+
+export default function HomePage() {
+  const products = getProducts();
+  return (
+    <div>
+      <div className="page">
+        <div className="home-hero">
+          <h1 className="home-title">Welcome To One Shop</h1>
+          <p className="home-subtitle">Your one-stop shop for all your needs.</p>
+        </div>
+
+        <div className="container">
+          <h2 className="page-title">Our Products</h2>
+          <div className="product-grid">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
